2f89df0e61d6bb385613f098eb62e2bd333cfbbf
Nikunj-Trueline <nikunjtrueline@gmail.com>
2024-07-10 16:54:23 +0530

    constant update

---
+++ constant.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define pi 3.14
+// macro define
+
+int main()
+{
+
+    // int a = 10;
+
+    // a = 20;
+
+    // const float pi = 3.14;
+
+    // float pi = 7.89;
+
+    // pi = 8.90;
+
+    // pi = 9.0;
+
+    printf("The value of a is : %f\n", pi);
+
+    short int a = 32767;
+
+    return 0;
+}
