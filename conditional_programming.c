82cb3b73e73e3455f468ae9aa1643a79a49ca51d
Nikunj-Trueline <nikunjtrueline@gmail.com>
2024-07-23 17:27:56 +0530

    Conditional Programming

---
+++ conditional_programming.c
@@ -0,0 +1,75 @@
+/*
+
+if(condition)
+{
+// code run.....
+}
+
+........................
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    // printf("Programm for Conditional Programming..\n");
+
+    // int age;
+    // char gender;
+
+    // printf("Enter the age for check license Application.\n");
+    // scanf("%d", &age);
+
+    // printf("Enter a gender with single charcater ex.. M F O");
+    // scanf(" ");
+    // scanf("%c", &gender);
+
+    // if (age > 18 && gender == 'M')
+    // {
+    // }
+    // else
+    // {
+
+    //     printf("You are not eligble for apply license..");
+    // }
+
+    // int number;
+
+    // printf("Enter a number between 1 to 4.");
+    // scanf("%d", &number);
+
+    // switch (number)
+    // {
+    // case 1:
+    //     printf("You selected pizza.\n");
+    //     break;
+
+    // case 2:
+    //     printf("You selected Burger.\n");
+    //     break;
+
+    // case 3:
+    //     printf("You selected Sendwich.\n");
+    //     break;
+
+    // case 4:
+    //     printf("You selected Vadapau.\n");
+    //     break;
+
+    // default:
+    //     printf("Nothing...\n");
+    //     break;
+    // }
+
+    // printf("Thank you...");
+
+    // if (printf(""))
+    // {
+    //     printf("Run....");
+    // }
+
+    int d = printf("Nikunj Asodariya\n");
+
+    printf("%d", d);
+}
